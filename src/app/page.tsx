0f8cd98a6a73fb215dcd406bd9d58a3b573a1e18
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-03-27 23:29:08 +0700

    Refactor page layout and fix indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,39 @@ import { cn } from "@/lib/utils";
 
 export default async function HomePage() {
   return (
-    <main className="container py-10">
-      <h1 className="text-4xl font-bold text-center">
-        Triển khai sản phẩm &quot;Lò không khói của tiến sỹ Paul&quot;
-      </h1>
-      <div className="py-10 flex gap-5 items-center">
-        <picture>
-          <img
-            src="z5288918661266_d563c8c11eef5587f24e279e03f6932f.jpg"
-            alt=""
-          />
-        </picture>
-        <blockquote
-          className="tiktok-embed"
-          cite="https://www.tiktok.com/@novitagroup/video/7349120696703864066"
-          data-video-id="7349120696703864066"
-        >
-          <section>
-            <a
-              target="_blank"
-              title="@novitagroup"
-              href="https://www.tiktok.com/@novitagroup?refer=embed"
-            >
-              @novitagroup
-            </a>
-          </section>
-        </blockquote>
-        <script async src="https://www.tiktok.com/embed.js"></script>
-      </div>
-      <FAQ />
-      <div className="text-center py-32">
-        <Button>Đăng ký tham gia sử dụng &quot;Lò không khói&quot;</Button>
+    <main>
+      <div className="container py-10">
+        <h1 className="text-4xl font-bold text-center">
+          Triển khai sản phẩm &quot;Lò không khói của tiến sỹ Paul&quot;
+        </h1>
+        <div className="py-10 flex gap-5 items-center">
+          <picture>
+            <img
+              src="z5288918661266_d563c8c11eef5587f24e279e03f6932f.jpg"
+              alt=""
+            />
+          </picture>
+          <blockquote
+            className="tiktok-embed"
+            cite="https://www.tiktok.com/@novitagroup/video/7349120696703864066"
+            data-video-id="7349120696703864066"
+          >
+            <section>
+              <a
+                target="_blank"
+                title="@novitagroup"
+                href="https://www.tiktok.com/@novitagroup?refer=embed"
+              >
+                @novitagroup
+              </a>
+            </section>
+          </blockquote>
+          <script async src="https://www.tiktok.com/embed.js"></script>
+        </div>
+        <FAQ />
+        <div className="text-center py-32">
+          <Button>Đăng ký tham gia sử dụng &quot;Lò không khói&quot;</Button>
+        </div>
       </div>
       <footer
         className={cn(
