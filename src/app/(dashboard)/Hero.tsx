c3ab31c5daf9e7b00c28d29b2a4bc051b22a3c9b
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-03-28 10:47:51 +0700

    Update Hero component layout

--- src/app/(dashboard)/Hero.tsx
+++ src/app/(dashboard)/Hero.tsx
@@ -4,7 +4,7 @@ import { TikTokEmbed } from "react-social-media-embed";
 
 export default function Hero() {
   return (
-    <div className="py-10 flex gap-5 items-center">
+    <div className="py-10 flex flex-col md:flex-row gap-5 items-center">
       <picture>
         <img src="z5288918661266_d563c8c11eef5587f24e279e03f6932f.jpg" alt="" />
       </picture>
