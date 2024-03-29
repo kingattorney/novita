64323e336a47165417e1aa347fbb900e9e72e15e
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-03-29 17:30:08 +0700

    Update content and add links in Thungo component

--- src/app/(dashboard)/ThuNgo.tsx
+++ src/app/(dashboard)/ThuNgo.tsx
@@ -11,8 +11,30 @@ export default async function ThuNgo() {
       <h2 className="text-4xl  font-medium text-center font-itim text-[#3FA514]">
         Thư ngỏ
       </h2>
-      <div className="bg-white flex flex-col prose  p-5 rounded-xl max-w-screen-lg border border-[#71AE0F]">
+      <div className="bg-white flex  flex-col prose  p-5 rounded-xl max-w-screen-lg border border-[#71AE0F]">
         <DocumentRenderer document={await homepage!.thungo()} />
+        <div>
+          <p>
+            King Attorney
+            <br />
+            <a href="https://kaapp.net/">https://kaapp.net</a>
+          </p>
+          <p>
+            Ka-Uni
+            <br />
+            <a href=" https://www.kauni.ai/"> https://kauni.ai</a>
+          </p>
+          <p>
+            An Bình 
+            <br />
+            10:30am of 03 March 24
+          </p>
+          <p>
+            TS. LS Đỗ Hữu Chiến 
+            <br />
+            (JD Martin Do)
+          </p>
+        </div>
       </div>
     </div>
   );
