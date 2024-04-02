7d81f498783299f1c256cbad4aa254928515ef79
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-04-02 17:43:52 +0700

    Update register button label to "Gasifier"

--- locales/en.ts
+++ locales/en.ts
@@ -3,7 +3,7 @@ export default {
   faq: "FAQ",
   view_more: "View more",
   open_letter: "Open letter",
-  register_button: 'Register to use "Smokeless stove"',
+  register_button: 'Register to use "Gasifier"',
   contact_info: "Contact information",
   contact_address: "Address",
   contact_phone: "Phone",
