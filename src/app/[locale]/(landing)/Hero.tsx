111bac59b899858064f97e249c9b0400baa75c19
Th1nhNg0 <ngophuthinh2001@gmail.com>
2024-04-12 10:55:53 +0700

    Update package.json and pnpm-lock.yaml to include embla-carousel-react version 8.0.2

--- src/app/[locale]/(landing)/Hero.tsx
+++ src/app/[locale]/(landing)/Hero.tsx
@@ -1,19 +1,44 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Link from "next/link";
 import React from "react";
 import { TikTokEmbed } from "react-social-media-embed";
 import { useCurrentLocale, useI18n } from "../../../../locales/client";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export default function Hero() {
   const t = useI18n();
   const locale = useCurrentLocale();
+
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
     <div className="container py-10">
       <h2 className="text-4xl font-medium text-center font-itim text-[#3FA514]">
         {t("product_info")}
       </h2>
-      <div className="py-10 flex flex-col justify-center  md:flex-row gap-5 items-center">
+      <div className="py-10 flex flex-col gap-20 justify-center  md:flex-row gap-5 items-center">
         <picture className="w-full">
           <img
             src={`heroimage_${locale}.png`}
@@ -21,10 +46,32 @@ export default function Hero() {
             alt=""
           />
         </picture>
-        <TikTokEmbed
-          url="https://www.tiktok.com/@novitagroup/video/7349120696703864066"
-          className="w-full"
-        />
+        <div>
+          <Carousel setApi={setApi}>
+            <CarouselContent>
+              <CarouselItem>
+                <TikTokEmbed url="https://www.tiktok.com/@novitagroup/video/7349120696703864066" />
+              </CarouselItem>
+              <CarouselItem>
+                <TikTokEmbed url="https://www.tiktok.com/@novitagroup/video/7356818643902008593" />
+              </CarouselItem>
+            </CarouselContent>
+          </Carousel>
+          <div className="py-2 text-center text-sm text-muted-foreground">
+            {Array.from({ length: count }, (_, i) => (
+              <span
+                key={i}
+                onClick={() => api?.scrollTo(i)}
+                className={cn(
+                  "cursor-pointer mx-1 p-2 rounded drop-shadow",
+                  i === current - 1 ? "bg-[#3FA514] text-white" : "bg-gray-200"
+                )}
+              >
+                {i + 1}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
       <div className="flex justify-center">
         <Button
